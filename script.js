c30500a90837631bda953a012044868fa599b6b2
jensby <jonas.ensby@gmail.com>
2019-10-09 22:00:21 +0200

    Flip function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 let moves = 0;
 let wins = 0;
+let flipped = false;
+let curCard = '';
 
 
 // Shuffle
@@ -10,17 +12,47 @@ function shuffleCards() {
 	for (i = 0; i < cardList.length; i++) { 
 		let card = cardList[i];
 		let pos = Math.floor(Math.random() * 12); 
-		//card.classList.add(" flex-"+pos);	
 		card.style.order=pos;
+		card.addEventListener('click', flipCard);
 	}
 	return; 
 }
 
 // Flip
 
+function flipCard() {
+	this.classList.add('show');
+	if (!flipped) {
+		curCard = this.classList[1];
+		flipped = true;
+	}
+	else {
+		if (this.classList[1] === curCard) {
+			let correct = document.querySelectorAll('.'+curCard);
+			correct[0].classList.add('found');
+			correct[1].classList.add('found');
+			flipped = false;
+		}
+		else {
+			setTimeout(clearFlip, 2000);
+			function clearFlip() {
+				let wrong = document.querySelectorAll('.show');
+				for (i = 0; i < wrong.length; i++) { 
+					let card = wrong[i];
+					card.classList.remove("show")
+				}
+			}
+			flipped = false;
+		}
+	}
+}
+
 
 // Win
 
+
+let moves =+ 1;
+let wins =+ 1;
 
 //Init
 
