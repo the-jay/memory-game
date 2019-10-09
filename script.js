8747b1959deadb70dbf7ec3756858e0ff6d72390
jensby <jonas.ensby@gmail.com>
2019-10-09 23:11:51 +0200

    Score, Controls and Polish

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ let wins = 0;
 let flipped = false;
 let curCard = '';
 
-
 // Shuffle
 function shuffleCards() {
+	moves = 0;
+	wins = 0;
 	let cardList = document.querySelectorAll('.card');
 	for (i = 0; i < cardList.length; i++) { 
 		let card = cardList[i];
 		let pos = Math.floor(Math.random() * 12); 
+		card.classList.remove('found','show')
 		card.style.order=pos;
 		card.addEventListener('click', flipCard);
+		document.getElementById('moves').innerHTML = "Trekk: "+moves;
 	}
 	return; 
 }
@@ -22,7 +25,7 @@ function shuffleCards() {
 
 function flipCard() {
 	this.classList.add('show');
-	if (!flipped) {
+	if (!flipped) {  
 		curCard = this.classList[1];
 		flipped = true;
 	}
@@ -38,27 +41,33 @@ function flipCard() {
 		}
 		else {
 			setTimeout(clearFlip, 1000);
-			function clearFlip() {
-				let wrong = document.querySelectorAll('.show');
-				for (i = 0; i < wrong.length; i++) { 
-					let card = wrong[i];
-					card.classList.remove("show")
-				}
-			}
 			flipped = false;
 			moves += 1;
 		}
+		document.getElementById('moves').innerHTML = "Trekk: "+moves;
 	}
 	return; 
 }
 
 
+function clearFlip() {
+	let wrong = document.querySelectorAll('.show');
+	for (i = 0; i < wrong.length; i++) { 
+		let card = wrong[i];
+		card.classList.remove("show")
+	}
+}
 // Win
 	
 function checkWin() {
 	console.log(wins);
 	if (wins === 6) {
-		alert('Gratulerer!');
+		if (moves === 6) {
+		alert('Du klarte det på første forsøk. Du er enten synsk eller en juksemaker');
+		}
+		else {
+		alert('Gratulerer! Du brukte '+moves+' trekk for å vinne. Prøv igjen for å slå den');
+		}
 	}
 	return; 
 }
